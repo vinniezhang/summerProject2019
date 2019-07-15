0691f4389c6d29603b551f7067336b145bf27f18
vzhangEA <vzhang@edanalytics.org>
2019-07-15 13:19:04 -0500

    removed gitignore of build

---
+++ build/precache-manifest.ee30196bbf938d6d5ba1566f1f653fec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafa575a0e6eb3aa8beed817dc6338c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7bf19cca83c8fcd432e",
+    "url": "/static/js/2.5926afa6.chunk.js"
+  },
+  {
+    "revision": "d4a70b83d4501ad8313e",
+    "url": "/static/js/main.56aa7bc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
